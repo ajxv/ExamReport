a76014b7852ae12deb7b01d4b5461d38a171ade6
unknown <abin.xv@gmail.com>
2023-04-16 13:25:44 +0530

    file upload added

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import eel
 import wx
+import os
+import shutil
 
 # initializing the application  
 eel.init('web')
 
 @eel.expose
-def save_file(file):
-    pass
+def save_file(date, filepath, input_folder="input_files/"):
+
+    if not os.path.exists(input_folder):
+        os.mkdir(input_folder)
+    
+    filename = filepath.split(os.path.sep)[-1].replace(' ', '_')
+    save_path = os.path.join(input_folder, f"{date}_{filename}")
+
+    shutil.copyfile(filepath, save_path)
+
+    return True
 
 @eel.expose
 def handleFileUpload(wildcard="*.xlsx"):
--- web/js/script.js
+++ web/js/script.js
@@ -4,6 +4,19 @@ function getPathToFile() {
             $(".file-upload").addClass('active');
             $("#noFile").text(r.split("\\").at(-1).substring(0, 30));
             $("#filePath").text(r);
+            $('#submit-btn').removeAttr('disabled');
         }
     );    
 };
+
+function formSubmit() {
+    filepath = $("#filePath").text();
+    date = $("#date").val();
+
+    eel.save_file(date, filepath)
+
+    $('.alert').show()
+    
+    console.log(date)
+    console.log(filepath)
+}
